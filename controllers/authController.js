a5a51db03ee352bba12fb4468d64d22210c5a3ac
Katie Loesch <catherine.loesch@protonmail.com>
2025-06-02 00:04:16 +0100

    update server.js to fix cors issues

--- controllers/authController.js
+++ controllers/authController.js
@@ -43,6 +43,12 @@ export const login = async (req, res) => {
   //one day in milliseconds
   const oneDayMs = 1000 * 60 * 60 * 24;
 
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://hunting-buddy.katieloesch.co.uk'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
   // send back cookie, name of cookie: 'token',
   res.cookie('token', token, {
     httpOnly: true, // can't be accessed using JS, makes it more secure
