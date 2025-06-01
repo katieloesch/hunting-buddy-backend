f693d45be97c345e61ea2d4603351e28e1b13da6
Katie Loesch <catherine.loesch@protonmail.com>
2025-06-02 00:37:07 +0100

    update server.js to fix cors issues

--- server.js
+++ server.js
@@ -54,21 +54,9 @@ app.use(
   cors({
     origin: 'https://hunting-buddy.katieloesch.co.uk',
     credentials: true,
+    exposedHeaders: ['set-cookie'],
   })
 );
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  if (req.method === 'OPTIONS') {
-    req.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
 
 // app.options('*', cors(corsOptions));
 
